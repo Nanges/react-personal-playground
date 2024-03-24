6d20fec199a51ccc5a8a47bfac1b8a31c819c515
olivier.lahaye <olivier.lahaye@oniryx.be>
2024-03-24 19:00:54 +0100

    Setup strategy reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,60 @@
+import { Reducer, useReducer } from 'react';
 import { FilesButton } from './components/files-button';
 import { FilesDropzone } from './components/files-dropzone';
 
+type State = {
+    showMostUsed: boolean;
+    search: string;
+};
+
+interface ActionStrategy<T> {
+    reduce(state: T): Partial<T>;
+}
+
+class UpdateShowMostUsed implements ActionStrategy<State> {
+    constructor(readonly showMostUsed: boolean = false) {}
+
+    reduce() {
+        return {
+            showMostUsed: this.showMostUsed,
+        };
+    }
+}
+
+class UpdateSearch implements ActionStrategy<State> {
+    constructor(readonly search: string) {}
+
+    reduce() {
+        return {
+            search: this.search,
+        };
+    }
+}
+
+function strategyReducer<T>(state: T, action: ActionStrategy<T>): T {
+    return { ...state, ...action.reduce(state) };
+}
+
+function useStrategyReducer<T>(initialState: T) {
+    return useReducer<Reducer<T, ActionStrategy<T>>>(strategyReducer, initialState);
+}
+
 function App() {
+    const [state, dispatch] = useStrategyReducer<State>({ showMostUsed: false, search: '' });
+
     return (
         <>
-            <FilesButton onFilesSelected={console.log}>Select template</FilesButton>
-            <FilesDropzone onFilesSelected={console.log} onFilesDropped={console.log}>
-                Drop template here or <em>select it</em>
-            </FilesDropzone>
+            <div>
+                <FilesButton onFilesSelected={console.log}>Select template</FilesButton>
+                <FilesDropzone onFilesSelected={console.log} onFilesDropped={console.log}>
+                    Drop template here or <em>select it</em>
+                </FilesDropzone>
+                <button onClick={() => dispatch(new UpdateShowMostUsed())}>Show all</button>
+                <button onClick={() => dispatch(new UpdateShowMostUsed(true))}>Show most used</button>
+                <input type="checkbox" checked={state.showMostUsed} onChange={(e) => dispatch(new UpdateShowMostUsed(e.target.checked))} />
+                <input type="search" name="" id="" onChange={(e) => dispatch(new UpdateSearch(e.target.value))} />
+            </div>
+            <pre>{JSON.stringify(state, null, 4)}</pre>
         </>
     );
 }
